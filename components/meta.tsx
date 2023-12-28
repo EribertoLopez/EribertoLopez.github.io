9e3a27cf9f048785cd7baf019ac9a598c268d951
Eriberto Lopez <22200357+EribertoLopez@users.noreply.github.com>
2023-12-27 20:17:37 -0800

    fix: meta element image

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import myImage from '../public/favicon/acryclic_cells_rainbow_zoom_in.jpeg';
 
 const Meta = () => {
   return (
@@ -36,7 +36,7 @@ const Meta = () => {
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       /> */}
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta property="og:image" content={myImage.src} />
     </Head>
   )
 }
