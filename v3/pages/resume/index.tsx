72d78c637954b53379874f47dee708490fcbfc66
elopez75 <elopez75@gatech.edu>
2024-12-07 23:34:09 -0800

    update: moved resume component to own file and config

--- v3/pages/resume/index.tsx
+++ v3/pages/resume/index.tsx
@@ -1,31 +1,11 @@
-import Container from '../../components/container'
-import Layout from '../../components/layout'
-
-import Head from 'next/head'
-
-import myImage from "../../public/favicon/IBelieveICanShibe_edited_50x50.png";
-
-import styles from './resume.module.css'
-import Image from "next/image";
 import { getAllResumes } from '../../lib/api';
-// import { Props } from 'next/script';
-import { useState } from 'react';
 import SidebarLayout from '../../components/SidebarLayout';
 import { Sections } from '../../lib/ConfigUtils';
 import Post from '../../interfaces/post'
 import Intro from '../../components/intro';
-import { Github, Linkedin, Globe } from 'lucide-react'
-import { resumeData } from './resumeConfig';
 import Resume from '../../components/Resume';
 
 type Props = { currentTheme: Sections, allPosts: Post[] }
-
-// type Props = {
-//   post: ProjectType
-//   morePosts: ProjectType[]
-//   preview?: boolean
-//   currentTheme: Sections
-// }
 
 
 const Content = ({ currentTheme, allPosts }: Props) => {
@@ -38,18 +18,13 @@ const Content = ({ currentTheme, allPosts }: Props) => {
       {heroPost && (
         <Resume />
       )}
-
     </div>
   )
 }
 
-export default function Index({ allPosts }: Props) {
+export default function Index({ currentTheme = Sections.Home, allPosts }: Props) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1).filter((post) =>  post.isPublished === true )
-  const [currentTheme, setCurr_entTheme] = useState<Sections>(Sections.Resume);
-  // const handleThemeChange = useCallback((theme: Sections) => {
-  //   setCurrentTheme(themes[theme]);
-  // }, [themes])
 
   return (
     <SidebarLayout
